5ae8235330c21f3c7ee112823bca26e88bf79da6
chanha-ryu <chanha.ryu@samsung.com>
2024-11-19 16:22:31 +0900

    wait show list.

--- content.js
+++ content.js
@@ -11,6 +11,27 @@ const LOGIN_PAGE_URI = 'https://etk.srail.kr/cmc/01/selectLoginForm.do?pageId=TK
 
 const isLogin = () => !!document.querySelectorAll(".login_wrap.val_m.fl_r > span").length;
 
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                observer.disconnect();
+                resolve(document.querySelector(selector));
+            }
+        });
+
+        // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
 if (document.URL.substring(0, dsturl1.length) == dsturl1 || document.URL.substring(0, dsturl2.length) == dsturl2 || document.URL.substring(0, dsturl3.length) == dsturl3) {
 
 	$(document).ready(function () {
@@ -48,126 +69,130 @@ if (document.URL.substring(0, dsturl1.length) == dsturl1 || document.URL.substri
     	vertical-align: middle;\
     }")
 			.appendTo("body");
-
-		// Inserts the macro button into the table.
-		if ($("#search-list").length != 0) {
-			var rows = $('#search-list table tr');
-			for (i = 1; i < rows.length; i++) {
-				var columns = $(rows[i]).children('td');
-				var first = $(columns[5]);
-				var coach = $(columns[6]);
-				var wait = $(columns[7]);
-				if (coach.children().length > 0) {
-					coach.append($("<p class='p5'></p>"));
-					var checkbox = $("<label></label>").html('<input type="checkbox" name="checkbox" class="coachMacro" value="' + i + '"> 매크로');
-					checkbox.children('input').prop('checked', coachSelected.indexOf(i + "") > -1);
-					coach.append(checkbox);
-				}
-				if (first.children().length > 0) {
-					first.append($("<p class='p5'></p>"));
-					var checkbox = $("<label></label>").html('<input type="checkbox" name="checkbox" class="firstMacro" value="' + i + '"> 매크로');
-					checkbox.children('input').prop('checked', firstSelected.indexOf(i + "") > -1);
-					first.append(checkbox);
-				}
-				if (wait.children().length > 0) {
-					wait.append($("<p class='p5'></p>"));
-					var checkbox = $("<label></label>").html('<input type="checkbox" name="checkbox" class="waitMacro" value="' + i + '"> 매크로');
-					checkbox.children('input').prop('checked', waitSelected.indexOf(i + "") > -1);
-					wait.append(checkbox);
-				}
-			}
-		}
-
-		if (sessionStorage.getItem('macro') == "true") {
-			// Restores user preferences
-			$("#psgInfoPerPrnb1").val(sessionStorage.getItem('psgInfoPerPrnb1'));
-			$("#psgInfoPerPrnb5").val(sessionStorage.getItem('psgInfoPerPrnb5'));
-			$("#psgInfoPerPrnb4").val(sessionStorage.getItem('psgInfoPerPrnb4'));
-			$("#psgInfoPerPrnb2").val(sessionStorage.getItem('psgInfoPerPrnb2'));
-			$("#psgInfoPerPrnb3").val(sessionStorage.getItem('psgInfoPerPrnb3'));
-			$("#locSeatAttCd1").val(sessionStorage.getItem('locSeatAttCd1'));
-			$("#rqSeatAttCd1").val(sessionStorage.getItem('rqSeatAttCd1'));
-
+		
+		
+		waitForElm("#search-list table tr").then(() => {
+			
+			// Inserts the macro button into the table.
 			if ($("#search-list").length != 0) {
 				var rows = $('#search-list table tr');
-
-				var succeed = false;
 				for (i = 1; i < rows.length; i++) {
 					var columns = $(rows[i]).children('td');
-
 					var first = $(columns[5]);
 					var coach = $(columns[6]);
 					var wait = $(columns[7]);
-
-					if (coachSelected.indexOf(i + "") > -1) {
-						var coachSpecials = coach.children("a");
-						if (coachSpecials.length != 0) {
-							for (j = 0; j < coachSpecials.length; j++) {
-								name = $(coachSpecials[j]).attr('class');
-								if (name == 'btn_small btn_burgundy_dark val_m wx90') {
-									$(coachSpecials[0])[0].click();
-									succeed = true;
-									break;
-								}
-							}
-							if (succeed == true) break;
-						}
+					if (coach.children().length > 0) {
+						coach.append($("<p class='p5'></p>"));
+						var checkbox = $("<label></label>").html('<input type="checkbox" name="checkbox" class="coachMacro" value="' + i + '"> 매크로');
+						checkbox.children('input').prop('checked', coachSelected.indexOf(i + "") > -1);
+						coach.append(checkbox);
 					}
-
-					if (firstSelected.indexOf(i + "") > -1) {
-						var firstSpecials = first.children("a");
-						if (firstSpecials.length != 0) {
-							for (j = 0; j < firstSpecials.length; j++) {
-								name = $(firstSpecials[j]).attr('class');
-								if (name == 'btn_small btn_burgundy_dark val_m wx90') {
-									$(firstSpecials[0])[0].click();
-									succeed = true;
-									break;
-								}
-							}
-							if (succeed == true) break;
-						}
+					if (first.children().length > 0) {
+						first.append($("<p class='p5'></p>"));
+						var checkbox = $("<label></label>").html('<input type="checkbox" name="checkbox" class="firstMacro" value="' + i + '"> 매크로');
+						checkbox.children('input').prop('checked', firstSelected.indexOf(i + "") > -1);
+						first.append(checkbox);
 					}
-
-					if (waitSelected.indexOf(i + "") > -1) {
-						var waitSpecials = wait.children("a");
-						if (waitSpecials.length != 0) {
-							for (j = 0; j < waitSpecials.length; j++) {
-								name = $(waitSpecials[j]).attr('class');
-								if (name == 'btn_small btn_burgundy_dark val_m wx90') {
-									$(waitSpecials[0])[0].click();
-									succeed = true;
-									break;
-								}
-							}
-							if (succeed == true) break;
-						}
+					if (wait.children().length > 0) {
+						wait.append($("<p class='p5'></p>"));
+						var checkbox = $("<label></label>").html('<input type="checkbox" name="checkbox" class="waitMacro" value="' + i + '"> 매크로');
+						checkbox.children('input').prop('checked', waitSelected.indexOf(i + "") > -1);
+						wait.append(checkbox);
 					}
 				}
-
-				if (succeed == true) {
-					sessionStorage.removeItem('macro');
-					sessionStorage.removeItem('coachSelected');
-					sessionStorage.removeItem('firstSelected');
-					sessionStorage.removeItem('waitSelected');
-					sessionStorage.removeItem('psgInfoPerPrnb1');
-					sessionStorage.removeItem('psgInfoPerPrnb5');
-					sessionStorage.removeItem('psgInfoPerPrnb4');
-					sessionStorage.removeItem('psgInfoPerPrnb2');
-					sessionStorage.removeItem('psgInfoPerPrnb3');
-					sessionStorage.removeItem('locSeatAttCd1');
-					sessionStorage.removeItem('rqSeatAttCd1');
-					chrome.extension.sendMessage({
-						type: 'playSound'
-					}, function (data) {});
-				} else {
-					setTimeout(function () {
-						location.reload();
-					}, 1000);
-				}
-			} else {
-				history.go(-1);
 			}
-		}
+	
+			if (sessionStorage.getItem('macro') == "true") {
+				// Restores user preferences
+				$("#psgInfoPerPrnb1").val(sessionStorage.getItem('psgInfoPerPrnb1'));
+				$("#psgInfoPerPrnb5").val(sessionStorage.getItem('psgInfoPerPrnb5'));
+				$("#psgInfoPerPrnb4").val(sessionStorage.getItem('psgInfoPerPrnb4'));
+				$("#psgInfoPerPrnb2").val(sessionStorage.getItem('psgInfoPerPrnb2'));
+				$("#psgInfoPerPrnb3").val(sessionStorage.getItem('psgInfoPerPrnb3'));
+				$("#locSeatAttCd1").val(sessionStorage.getItem('locSeatAttCd1'));
+				$("#rqSeatAttCd1").val(sessionStorage.getItem('rqSeatAttCd1'));
+	
+				if ($("#search-list").length != 0) {
+					var rows = $('#search-list table tr');
+	
+					var succeed = false;
+					for (i = 1; i < rows.length; i++) {
+						var columns = $(rows[i]).children('td');
+	
+						var first = $(columns[5]);
+						var coach = $(columns[6]);
+						var wait = $(columns[7]);
+	
+						if (coachSelected.indexOf(i + "") > -1) {
+							var coachSpecials = coach.children("a");
+							if (coachSpecials.length != 0) {
+								for (j = 0; j < coachSpecials.length; j++) {
+									name = $(coachSpecials[j]).attr('class');
+									if (name == 'btn_small btn_burgundy_dark val_m wx90') {
+										$(coachSpecials[0])[0].click();
+										succeed = true;
+										break;
+									}
+								}
+								if (succeed == true) break;
+							}
+						}
+	
+						if (firstSelected.indexOf(i + "") > -1) {
+							var firstSpecials = first.children("a");
+							if (firstSpecials.length != 0) {
+								for (j = 0; j < firstSpecials.length; j++) {
+									name = $(firstSpecials[j]).attr('class');
+									if (name == 'btn_small btn_burgundy_dark val_m wx90') {
+										$(firstSpecials[0])[0].click();
+										succeed = true;
+										break;
+									}
+								}
+								if (succeed == true) break;
+							}
+						}
+	
+						if (waitSelected.indexOf(i + "") > -1) {
+							var waitSpecials = wait.children("a");
+							if (waitSpecials.length != 0) {
+								for (j = 0; j < waitSpecials.length; j++) {
+									name = $(waitSpecials[j]).attr('class');
+									if (name == 'btn_small btn_burgundy_dark val_m wx90') {
+										$(waitSpecials[0])[0].click();
+										succeed = true;
+										break;
+									}
+								}
+								if (succeed == true) break;
+							}
+						}
+					}
+	
+					if (succeed == true) {
+						sessionStorage.removeItem('macro');
+						sessionStorage.removeItem('coachSelected');
+						sessionStorage.removeItem('firstSelected');
+						sessionStorage.removeItem('waitSelected');
+						sessionStorage.removeItem('psgInfoPerPrnb1');
+						sessionStorage.removeItem('psgInfoPerPrnb5');
+						sessionStorage.removeItem('psgInfoPerPrnb4');
+						sessionStorage.removeItem('psgInfoPerPrnb2');
+						sessionStorage.removeItem('psgInfoPerPrnb3');
+						sessionStorage.removeItem('locSeatAttCd1');
+						sessionStorage.removeItem('rqSeatAttCd1');
+						chrome.extension.sendMessage({
+							type: 'playSound'
+						}, function (data) {});
+					} else {
+						setTimeout(function () {
+							location.reload();
+						}, 1000);
+					}
+				} else {
+					history.go(-1);
+				}
+			}
+		});
 	});
 }
